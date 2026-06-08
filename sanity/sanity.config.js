8cbdf87df4b99e086c89300df2a12435467016f3
John Agostino <john@agostinoindustries.com>
2026-06-07 21:34:09 -0400

    Add duplicate action to Sanity Studio

--- sanity/sanity.config.js
+++ sanity/sanity.config.js
@@ -1,27 +1,31 @@
-import { defineConfig } from 'sanity'
+import { defineConfig, useClient } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemaTypes'
-import {
-  PublishAction,
-  DiscardChangesAction,
-  DeleteAction,
-  DuplicateAction,
-} from 'sanity'
+
+function DuplicateAction({ id, type, onComplete }) {
+  const client = useClient({ apiVersion: '2024-01-01' })
+  return {
+    label: 'Duplicate',
+    onHandle: async () => {
+      const doc = await client.getDocument(id)
+      if (!doc) return
+      const { _id, _rev, _updatedAt, _createdAt, ...rest } = doc
+      await client.create({ ...rest, _type: type })
+      alert('Duplicated! Check the top of the document list.')
+      onComplete()
+    }
+  }
+}
 
 export default defineConfig({
   name: 'default',
   title: 'Itsurwiener',
   projectId: 'dwoifffe',
   dataset: 'production',
-  plugins: [
-    structureTool(),
-    visionTool(),
-  ],
+  plugins: [structureTool(), visionTool()],
   document: {
-    actions: () => [PublishAction, DuplicateAction, DiscardChangesAction, DeleteAction],
+    actions: (prev) => [...prev, DuplicateAction],
   },
-  schema: {
-    types: schemaTypes,
-  },
+  schema: { types: schemaTypes },
 })
