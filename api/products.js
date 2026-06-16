824b04d88723de4f6775e9b3e2c7a2a90ce80f1a
John Agostino <john@agostinoindustries.com>
2026-06-15 21:35:58 -0400

    Add Snipcart product validation API

---
+++ api/products.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  const { id, price, name } = req.query
+  const html = `<!DOCTYPE html>
+<html><body>
+  <button class="snipcart-add-item"
+    data-item-id="${id}"
+    data-item-price="${price}"
+    data-item-url="/api/products?id=${id}&price=${price}&name=${name || id}"
+    data-item-name="${name || id}"
+  >${name || id}</button>
+</body></html>`
+  res.setHeader('Content-Type', 'text/html')
+  res.status(200).send(html)
+}
